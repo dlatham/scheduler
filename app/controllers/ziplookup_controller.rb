665593e0c75395ddc3c26ca77e56ec019d4fbe00
Dave <dave.latham@gmail.com>
2014-09-04 18:38:49 -0700

    Styling and ajax zip check

--- app/controllers/ziplookup_controller.rb
+++ app/controllers/ziplookup_controller.rb
@@ -1,13 +1,15 @@
 class ZiplookupController < ApplicationController
 	include Geokit::Geocoders
 	def show
-		client = GeonamesGeocoder.geocode(params[:id])
-		home = GeonamesGeocoder.geocode('94115')
-		@result = client.distance_from(home).round(2)
-  end
-
-  def showCoverage
-    client = GeonamesGeocoder.geocode(params[:zip])
-
+		@client = GeonamesGeocoder.geocode(params[:id])
+		@distance = @client.distance_from([37.777,-122.445]).round(2) #home latitude and longitude
+    if @distance < 15 then
+      @result = {"service" => "full", "city" => @client.city, "state" => @client.state, "distance" => @distance}
+    elsif @distance < 30 then
+      @result = @result = {"service" => "limited", "city" => @client.city, "state" => @client.state, "distance" => @distance}
+    else
+      @result = @result = {"service" => "none", "city" => @client.city, "state" => @client.state, "distance" => @distance}
+    end
+    render json: @result
   end
 end
